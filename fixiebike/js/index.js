eccd8146c30c4ecb24e125329411594498ea8bd1
viankakrisna <viankakrisna@gmail.com>
2015-01-19 02:02:02 +0700

    Fourth Release
    Added several files

---
+++ fixiebike/js/index.js
@@ -0,0 +1,59 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$.stellar();
+
+$(function() {
+
+    var $window = $(window);
+    var scrollTime = 0.5;
+    var scrollDistance = 170;
+
+    $window.on("mousewheel DOMMouseScroll", function(event) {
+
+        event.preventDefault();
+
+        var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+        TweenMax.to($window, scrollTime, {
+            scrollTo: {
+                y: finalScroll,
+                autoKill: true
+            },
+            ease: Power1.easeOut,
+            overwrite: 5
+        });
+
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.pager a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.pager a').removeClass("active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
+}
+$(document).ready(function() {
+    $(document).on("scroll", onScroll);
+})
