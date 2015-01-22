7baa258e6e0b8be82f337813f17c3eca232c65a6
viankakrisna <viankakrisna@gmail.com>
2015-01-22 10:09:06 +0700

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,9 +1,4 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
+$(document).ready(function(){
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -30,4 +25,5 @@ $(function() {
       }
     }
   });
+});
 });
